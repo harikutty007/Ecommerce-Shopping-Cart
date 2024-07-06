73acd2984fd56b7edcd8e8d69ac2c933edcc5379
Anjitha P <anjuanjitha30@gmail.com>
2024-07-06 14:11:24 +0530

    second commit

--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def store(request):
+    context = {}
+    return render(request,'ecommerce_app/store.html')
+def cart(request):
+    context = {}
+    return render(request,'ecommerce_app/cart.html')
+def checkout(request):
+    context = {}
+    return render(request,'ecommerce_app/checkout.html')
