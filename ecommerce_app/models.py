1f572c516a5733d32ba1e46e22df2907df54b518
Anjitha P <anjuanjitha30@gmail.com>
2024-07-06 21:21:41 +0530

    second commit

--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -14,8 +14,8 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
-    digital = models.BooleanField(default=False, null=True, blank=True)
-    
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True,blank=True)
     def __str__(self):
         return self.name  
     
